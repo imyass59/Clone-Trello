25a18d0c33e760f05a7e4b5ddfd18daba057b828
Ilyass Elkhadiri <m.imyass.59@gmail.com>
2023-06-11 11:28:46 +0100

    add logic

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react';
+
+const useLocalStorage = (key, initialValue) => {
+  const [value, setValue] = useState(() => {
+    const storedValue = window.localStorage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : initialValue;
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+};
+
+export default useLocalStorage;
